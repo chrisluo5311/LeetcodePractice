157f6195bc08d513249cb028cb3f8c6d5121de4b
Chris Luo <chrislo5311@gmail.com>
2023-04-24 02:50:27 +0800

    add 3SumClosest & 4Sum & RemoveDuplicate

---
+++ java_chris/array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,22 @@
+package array;
+
+public class RemoveDuplicatesFromSortedArray {
+
+
+    public static int removeDuplicates(int[] nums) {
+        int insertIndex = 1;
+        for (int i = 1; i<nums.length;++i){
+            if (nums[i] != nums[i-1]){
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
---
+++ java_chris/test/LeetCodeTest.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author chris
+ */
+public class LeetCodeTest {
+
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 10;i++){
+            System.out.println(i);
+        }
+    }
+
+}
