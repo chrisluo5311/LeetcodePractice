d2316a58687ed04f464013d9737c4e86d31f4c46
chrisluo5311 <chrislo5311@gmail.com>
2024-09-01 01:02:20 +0800

    update leetcode 2149

--- java_chris/array/RearrangeArray_2149.java
+++ java_chris/array/RearrangeArray_2149.java
@@ -1,4 +1,31 @@
 package array;
 
+import java.util.Arrays;
+
 public class RearrangeArray_2149 {
+    public static void main(String[] args) {
+        int[] output = rearrangeArray(new int[]{3,1,-2,-5,2,-4});
+        Arrays.stream(output).forEach(System.out::println);
+    }
+
+    public static int[] rearrangeArray(int[] nums) {
+        int numsLength = nums.length;
+        int[] posNums = new int[numsLength/2];
+        int[] negNums = new int[numsLength/2];
+        int indexPos = 0;
+        int indexNeg = 0;
+        for (int i : nums){
+            if (i < 0){
+                negNums[indexNeg++] = i;
+            } else {
+                posNums[indexPos++] = i;
+            }
+        }
+        int[] output = new int[numsLength];
+        for (int i = 0; i < numsLength; i+=2){
+            output[i] = posNums[i/2];
+            output[i+1] = negNums[i/2];
+        }
+        return output;
+    }
 }
