b71680e275e4e1353cf49d31ed5d3cc447fb83a6
chrisluo5311 <chrislo5311@gmail.com>
2024-09-14 00:50:24 +0800

    add leetcode 107

---
+++ java_chris/tree/BinaryTreeLevelOrderTraversal2_107.java
@@ -0,0 +1,32 @@
+package tree;
+
+import java.util.*;
+
+//就是把102題的結果反轉
+public class BinaryTreeLevelOrderTraversal2_107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()){
+            List<Integer> temp = new ArrayList<>();
+            int levelSize = queue.size();
+            for (int i = 0 ; i < levelSize ; i++){
+                TreeNode currNode = queue.poll();
+                temp.add(currNode.val);
+                if (currNode.left != null){
+                    queue.add(currNode.left);
+                }
+                if (currNode.right != null){
+                    queue.add(currNode.right);
+                }
+            }
+            ans.add(temp);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
