f1f35949f9445f8e96bce128b42fe788e354b8f5
chrisluo5311 <chrislo5311@gmail.com>
2024-09-14 00:13:07 +0800

    update QueueTest 註解

--- java_chris/queue/QueueTest.java
+++ java_chris/queue/QueueTest.java
@@ -3,42 +3,48 @@ package queue;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * ✅Boolean offer() – 添加成功返回 True；失敗返回 False。
+ * Boolean add(E e) – 添加成功返回 True；失敗則丟出 IllegalStateException。
+ * ✅Object poll() – 刪除成功返回第一個 element；失敗返回 null。
+ * Object remove() – 刪除成功返回第一個 element；失敗（當queue為空）則丟出 NoSuchElementException。
+ * ✅Object peek() – 返回第一個 element，當 queue 為空返回 null。
+ * Object element() – 返回第一個 element，當 queue 為空會丟出 NoSuchElementException。
+ * */
 public class QueueTest {
 
     public static void main(String[] args) {
-        /**
-         * ✅Boolean offer() – 添加成功返回 True；失敗返回 False。
-         * Boolean add(E e) – 添加成功返回 True；失敗則丟出 IllegalStateException。
-         * ✅Object poll() – 刪除成功返回第一個 element；失敗返回 null。
-         * Object remove() – 刪除成功返回第一個 element；失敗（當queue為空）則丟出 NoSuchElementException。
-         * ✅Object peek() – 返回第一個 element，當 queue 為空返回 null。
-         * Object element() – 返回第一個 element，當 queue 為空會丟出 NoSuchElementException。
-         * */
-
         Queue<Integer> queue = new LinkedList<Integer>();
 
+        //offer & add
         queue.offer(1);
         queue.add(2);
-        System.out.println(queue);
+        System.out.println("queue: "+queue);
 
+        //remove & poll
         Integer a = queue.remove();
         System.out.println("remove: "+a);
-        System.out.println(queue);
+        System.out.println("queue: "+queue);
         Integer b = queue.poll();
         System.out.println("poll: "+b);
-        System.out.println(queue);
-        System.out.println(queue.isEmpty());
+        System.out.println("queue: "+queue);
+
+        //isEmpty
+        System.out.println("queue isEmpty: "+queue.isEmpty());
 
         queue.offer(3);
         queue.offer(4);
         queue.offer(5);
         queue.offer(6);
+        System.out.println("queue: "+queue);
 
+        //element & peek
         Integer c = queue.element();
         Integer d = queue.peek();
         System.out.println("element: "+c);
         System.out.println("peek: "+d);
 
-        System.out.println(queue.size());
+        //size
+        System.out.println("queue size: "+queue.size());
     }
 }
