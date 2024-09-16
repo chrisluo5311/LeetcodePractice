34febc860e0792e7b65cf9c904d13c75dc2587d9
chrisluo5311 <chrislo5311@gmail.com>
2024-09-17 02:17:31 +0800

    update doubly linked list methods
    1. createDoublyLinkedList
    2. insertMiddle
    3. deleteMiddle
    4. deleteLastOne

--- java_chris/linked_list/doubly_linked_list/DoublyListNodeTest.java
+++ java_chris/linked_list/doubly_linked_list/DoublyListNodeTest.java
@@ -18,7 +18,73 @@ public class DoublyListNodeTest {
         return head;
     }
 
+    public static DoublyListNode insertMiddle(DoublyListNode head, int target, int data) {
+        DoublyListNode newNode = new DoublyListNode(data);
+        DoublyListNode curr = head;
+        while (curr != null) {
+            if (curr.val == target) {
+                // newNode first
+                newNode.next = curr.next;
+                newNode.prev = curr;
+                // nextNode & currNode
+                curr.next.prev = newNode;
+                curr.next = newNode;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    public static DoublyListNode deleteMiddle(DoublyListNode head, int target) {
+        DoublyListNode curr = head;
+        DoublyListNode prev = head;
+        while (curr != null) {
+            if (curr.val == target) {
+                prev.next = curr.next;
+                curr.next.prev = prev;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    public static DoublyListNode deleteLastOne(DoublyListNode head) {
+        DoublyListNode curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.prev.next = null;
+        return head;
+    }
+
     public static void main(String[] args) {
         DoublyListNode head = createDoublyLinkedList(new int[]{1, 2, 3, 4, 5});
+
+        // insert data in the middle of the doubly linked list
+        System.out.println("insert data:");
+        int data = 100;
+        DoublyListNode result = insertMiddle(head, 3, data);
+        while (result != null) {
+            System.out.print(result.val+", ");
+            result = result.next;
+        }
+
+        System.out.println("\ndelete data:");
+        // delete data in the middle of the doubly linked list
+        int target = 100;
+        DoublyListNode afterDeletionResult = deleteMiddle(head, target);
+        while (afterDeletionResult != null) {
+            System.out.print(afterDeletionResult.val+", ");
+            afterDeletionResult = afterDeletionResult.next;
+        }
+
+        // delete last one
+        System.out.println("\ndelete last one:");
+        DoublyListNode afterDeletingLastOne = deleteLastOne(head);
+        while (afterDeletingLastOne != null) {
+            System.out.print(afterDeletingLastOne.val+", ");
+            afterDeletingLastOne = afterDeletingLastOne.next;
+        }
     }
 }
