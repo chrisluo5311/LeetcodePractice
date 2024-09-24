f04661ca9d713f23155b8fc9e1163517bd36246a
chrisluo5311 <chrislo5311@gmail.com>
2024-09-24 10:29:49 -0700

    add leetcode 128

---
+++ java_chris/HashSet/LongestConsecutiveSequence_128.java
@@ -0,0 +1,29 @@
+package HashSet;
+
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence_128 {
+
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : nums){
+            set.add(i);
+        }
+
+        int seq = 0;
+        for (int i : set) {
+            if (set.contains(i-1)){
+                continue;
+            }
+            int tmpIndex = i+1;
+            int tmpCount = 1;
+            while(set.contains(tmpIndex)) {
+                tmpCount++;
+                tmpIndex++;
+            }
+            seq = Math.max(seq, tmpCount);
+        }
+        return seq;
+    }
+
+}
