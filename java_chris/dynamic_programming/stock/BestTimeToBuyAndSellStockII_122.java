35b336ffd9b5e7983ceb02aedda2bed64da5d688
chrisluo5311 <chrislo5311@gmail.com>
2025-04-23 23:26:18 -0700

    add dp 122
    BestTimeToBuyAndSellStockII

--- java_chris/dynamic_programming/stock/BestTimeToBuyAndSellStockII_122.java
+++ java_chris/dynamic_programming/stock/BestTimeToBuyAndSellStockII_122.java
@@ -2,28 +2,24 @@ package dynamic_programming.stock;
 
 /**
  *
- *  k = 1 in this problem
+ *  k = infinity in this problem
  *
  * */
-public class BestTimeToBuyAndSellStock_121 {
+public class BestTimeToBuyAndSellStockII_122 {
     public static int maxProfit(int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n][2];
         for (int i = 0 ; i < n ; i++) {
-            if (i-1 < 0) {
-                // no stock
+            if (i-1 == -1) {
                 dp[i][0] = 0;
-                // with stock
                 dp[i][1] = -prices[i];
                 continue;
             }
-            // no holding stock: max(reset, buy)
+            // no stock: max(reset, sell stock)
             dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            // hold stock: max(reset, sell= ((dp[i-1][0][1]=0) - prices[i])
-            dp[i][1] = Math.max(dp[i-1][1],  -prices[i]);
-
+            // own stock: max(reset, buy stock)
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
         }
-
         return dp[n-1][0];
     }
 
