d4e6bb49467d4284f2f98332073147255aee66c1
chrisluo5311 <chrislo5311@gmail.com>
2025-10-28 15:21:27 -0700

    Add example in LeetCode 133 CloneGraph

--- java_chris/graph/CloneGraph_133.java
+++ java_chris/graph/CloneGraph_133.java
@@ -38,4 +38,26 @@ public class CloneGraph_133 {
         }
         return cloneNode;
     }
+
+    public static void main(String[] args) {
+        // [[2,4],[1,3],[2,4],[1,3]]
+        CloneGraph_133 cloneGraph = new CloneGraph_133();
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        // neighbors
+        node1.neighbors.add(node2);
+        node1.neighbors.add(node4);
+
+        node2.neighbors.add(node1);
+        node2.neighbors.add(node3);
+
+        node3.neighbors.add(node2);
+        node3.neighbors.add(node4);
+
+        node4.neighbors.add(node1);
+        node4.neighbors.add(node3);
+        Node cloneNode = cloneGraph.cloneGraph(node1);
+    }
 }
