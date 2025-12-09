fd71ec7796b3bcbb63e8d683cde55f317745e0c4
chrisluo5311 <chrislo5311@gmail.com>
2025-12-08 20:12:15 -0800

    Update leetcode 124

--- java_chris/tree/leetcode/BinaryTreeMaximumPathSum_124.java
+++ java_chris/tree/leetcode/BinaryTreeMaximumPathSum_124.java
@@ -3,41 +3,21 @@ package tree.leetcode;
 import tree.TreeNode;
 
 public class BinaryTreeMaximumPathSum_124 {
-    static int sub_tree_max = Integer.MIN_VALUE;
-    public static int maxPathSum(TreeNode root) {
-        int res = dfs(root);
-        return Math.max(res, sub_tree_max);
+    int maxNum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return maxNum;
     }
 
-    static int dfs(TreeNode root) {
-        if (root == null) return -1001;
-        int left = dfs(root.left);
-        int mid_val = root.val;
-        int right = dfs(root.right);
+    int dfs(TreeNode node) {
+        if (node == null) return 0;
 
-        // calculate sub tree max
-        int tempSum = 0;
-        if (mid_val >= 0) {
-            tempSum += mid_val;
-            if (left >= 0) tempSum += left;
-            if (right >= 0) tempSum += right;
-        } else {
-            if (left < 0 && right < 0) {
-                tempSum = Math.max(mid_val, Math.max(left, right));
-            } else {
-                tempSum = mid_val + left + right;
-                tempSum = Math.max(tempSum, Math.max(left, right));
-            }
-        }
+        int leftVal = Math.max(0, dfs(node.left));
+        int rightVal = Math.max(0, dfs(node.right));
 
-        // post order
-        if (left < 0 && right < 0) return tempSum;
-        sub_tree_max = Math.max(sub_tree_max, tempSum);
-        mid_val += Math.max(left, right);
-        return mid_val;
-    }
+        int tmpVal = node.val + leftVal + rightVal;
+        maxNum = Math.max(tmpVal, maxNum);
 
-    public static void main(String[] args) {
-
+        return Math.max(leftVal, rightVal) + node.val;
     }
 }
