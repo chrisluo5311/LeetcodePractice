3eb4078272c9389b5f4cd7aaf5365f071a15a2bc
chrisluo5311 <chrislo5311@gmail.com>
2026-01-10 22:30:28 -0800

    Add leetcode 712

---
+++ java_chris/dynamic_programming/MinimumASCIIDeleteSumforTwoStrings_712.java
@@ -0,0 +1,49 @@
+package dynamic_programming;
+import java.util.Arrays;
+
+public class MinimumASCIIDeleteSumforTwoStrings_712 {
+    int[][] memo;
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        memo = new int[m][n];
+        for (int[] row: memo) {
+            Arrays.fill(row, -1);
+        }
+
+        return dp(s1, 0, s2, 0);
+    }
+
+    // dp is total cost => different character
+    int dp(String s1, int i, String s2, int j) {
+        int tmp = 0;
+        if (i == s1.length()) {
+            for (; j < s2.length() ; j++) {
+                tmp += s2.charAt(j);
+            }
+            return tmp;
+        }
+
+        if (j == s2.length()) {
+            for (; i < s1.length() ; i++) {
+                tmp += s1.charAt(i);
+            }
+            return tmp;
+        }
+
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            memo[i][j] = dp(s1, i+1, s2, j+1);
+        } else {
+            memo[i][j] = Math.min(
+                    s1.charAt(i) + dp(s1, i+1, s2, j),
+                    s2.charAt(j) + dp(s1, i, s2, j+1)
+            );
+        }
+
+        return memo[i][j];
+    }
+}
